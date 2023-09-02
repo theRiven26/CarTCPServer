7411d47c94c7ddc2b3d47d720248e786db62bdc4
Anrew Krivenko <demize26rus@gmail.com>
2023-09-03 01:22:46 +0300

    new comit

--- CarTCPClient/Program.cs
+++ CarTCPClient/Program.cs
@@ -18,8 +18,30 @@ namespace CarTCPClient
 
             var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
             var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            tcpSocket.Bind(tcpEndPoint);
-            tcpSocket.Listen(5);
+            Console.WriteLine("Ведите сообщение");
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+
+            byte[] buffer = new byte[256];
+            int dataSize = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                dataSize = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer,0,dataSize));
+            }
+            while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer.ToString());
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+
+            Console.ReadLine();
+
         }
     }
 }
